674c6a48d09802bf664cfdc9c95fc277c599a8b7
Bo-Cang <58157684+Bo-Cang@users.noreply.github.com>
2019-11-25 16:26:58 -0600

    Update server.py

--- server.py
+++ server.py
@@ -4,5 +4,12 @@ APP = bottle.Bottle()
 @APP.get('/')
 def index():
     return '<p>Hello</p>'
+@APP.get('/1/')
+def re():
+    return redirect("/hello/")
+@APP.get('/hello/')
+def index():
+    def hello1():
+    return '<p>Hello in hello 111</p>'
 if __name__ == '__main__':
     bottle.run(application=APP)
