06cf1ba7e3c480ed8350f8c3ba9d60b23dec697e
Bo-Cang <58157684+Bo-Cang@users.noreply.github.com>
2019-11-25 17:23:12 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 
-import bottle, redirect, route
+import bottle
 APP = bottle.Bottle()
 @APP.get('/')
 def index():
     return '<p>Hello</p>'
-@APP.route('/q')
+@APP.get('/q')
 def qq():
-    redirect('/h')
+    return redirect('/h')
 @APP.get('/h')
 def hello():
     return '<p>Hello in hello 111</p>'
