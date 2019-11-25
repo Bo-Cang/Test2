5df5ee64a33ae223624bca509aeae23ad32848f0
Bo-Cang <58157684+Bo-Cang@users.noreply.github.com>
2019-11-25 16:04:16 -0600

    Update and rename bottle2.py to bottle.py

---
+++ bottle.py
@@ -0,0 +1,8 @@
+
+import bottle
+APP = bottle.Bottle()
+@APP.get('/')
+def index():
+    return '<p>Hello</p>'
+if __name__ == '__main__':
+    bottle.run(application=APP)
--- bottle2.py
+++
@@ -1,7 +0,0 @@
-from bottle import route, run, template, request, static_file, redirect, bottle
-APP = bottle.Bottle()
-@APP.get('/')
-def index():
-    return 'hello world in hello'
-if __name__ == '__main__':
-    bottle.run(application=APP)
