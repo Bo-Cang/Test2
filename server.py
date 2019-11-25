164e0653003f096bf4c78590850fc7fbbd58b0fd
Bo-Cang <58157684+Bo-Cang@users.noreply.github.com>
2019-11-25 16:54:26 -0600

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ def index():
     return '<p>Hello</p>'
 @APP.get('/q')
 def qq():
-    return redirect("/h")
+    return redirect('/h')
 @APP.get('/h')
 def hello():
     return '<p>Hello in hello 111</p>'
