dcb4af953591a6c3c0f9c2a0721d19bcdd3000e8
Bo-Cang <58157684+Bo-Cang@users.noreply.github.com>
2019-11-25 16:00:56 -0600

    Update bottle2.py

--- bottle2.py
+++ bottle2.py
@@ -2,13 +2,6 @@ from bottle import route, run, template, request, static_file, redirect, bottle
 APP = bottle.Bottle()
 @APP.get('/')
 def index():
-    return "hello world in hello"
-    
-@APP.get('/1/')
-def HELLO():
-    return redirect('/2/')
-@APP.get('/2/')
-def hello2():
-    return "Hello world in hello2"
+    return 'hello world in hello'
 if __name__ == '__main__':
     bottle.run(application=APP)
