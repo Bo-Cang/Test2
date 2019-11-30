de50f286fcf5edf7f2432b815d8db37fd1943d89
Bo-Cang <58157684+Bo-Cang@users.noreply.github.com>
2019-11-29 18:53:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bottle import view
 import bottle
 from bottle import template
+import sqlite3
 APP = bottle.Bottle()
 @APP.get("/")
 def index():
@@ -11,6 +12,14 @@ def index():
 @APP.route("/q")
 def index():
     return template('index')
+    conn = sqlite3.connect('data5.db')
+    c = conn.cursor()
+    sql = "select * " \
+        + "from page_info  " \
+        + "where id in " + id_list + ";"
+    res = c.execute(sql)
+    res = [r for r in res]
+    return res
 if __name__ == '__main__':
     APP.run()
 
